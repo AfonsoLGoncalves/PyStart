d049d2369ec52bedb9bc73742c2a2221505ac85a
Afonso Santos <afonso.goncalves001@gmail.com>
2022-08-21 11:47:14 -0300

    booting gallows game

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,4 +3,25 @@ def jogar():
     print("Bem vindo ao jogo de adivinhação!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    # Enquanto não enforcou e não acertou
+    while (not enforcou and not acertou):
+        palpite = input("Qual letra?")
+        palpite = palpite.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (palpite.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+
+            index = index + 1
+        print("Jogando ...")
+
+
     print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
