bab94eff9837d1b421978d65a229ed255ee811fe
Afonso Santos <afonso.goncalves001@gmail.com>
2022-09-07 19:16:48 -0300

    Using List Comprehensions

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,26 +3,39 @@ def jogar():
     print("Bem vindo ao jogo de adivinhação!")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "quiabo".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta] # List Comprehensions
     print(letras_acertadas)
 
     # Condicionais
     enforcou = False
     acertou = False
+    erros = 0
 
     # Enquanto não enforcou e não acertou
     while (not enforcou and not acertou):
-        palpite = input("Qual letra?")
-        palpite = palpite.strip()
+        palpite = input("Qual letra? ")
+        palpite = palpite.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (palpite.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if palpite in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if palpite == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, você errou! Faltam {6 - erros} tentativas.")
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
         print(letras_acertadas)
 
+    if (acertou):
+        return print("Parabéns você acertou a palavra secreta")
+    else:
+        return print("Você perdeu! Não desanime continue tentando!")
     print("Fim do jogo")
 
 
