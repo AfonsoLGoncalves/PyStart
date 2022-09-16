e72e054f35e87f1b32cbc534b03bec5283ef7550
Afonso Santos <afonso.goncalves001@gmail.com>
2022-09-15 22:55:56 -0300

    Learn how to use dependecy injection

---
+++ POO/Aula 10 - Injeção de Dependência/pessoa.py
@@ -0,0 +1,32 @@
+from typing import Type
+
+
+class Casa:
+
+    def __init__(self) -> None:
+        self.__endereco = "Rua dos Limoeiros"
+
+    def acender_luzes(self) -> None:
+        print("Estou ascendendo as Luzes")
+
+    def get_endereco(self) -> str:
+        return self.__endereco
+
+# Classe pessoa encontra-se dependente de outra classe
+class Pessoa:
+
+    def __init__(self, nome: str, local: Type[Casa]) -> None:
+        self.__local = local
+        self.__nome = nome
+
+    def entrar_no_local(self) -> None:
+        self.__local.acender_luzes()
+
+    def apresentar_local (self) -> None:
+        endereco = self.__local.get_endereco()
+        print(endereco)
+
+minha_casa = Casa()
+ana = Pessoa("Ana", minha_casa)
+ana.apresentar_local()
+ana
