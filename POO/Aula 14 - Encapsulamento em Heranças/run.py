d36dc7b2e25ad0d7a8f3634e6d7d3cb4eb5b1ec1
Afonso Santos <afonso.goncalves001@gmail.com>
2022-09-19 22:28:39 -0300

    Learn how to use the Protected convention

---
+++ POO/Aula 14 - Encapsulamento em Heranças/run.py
@@ -0,0 +1,27 @@
+class DatabaseConn:
+
+    def __init__(self) -> None:
+        self.__database = "Postgres"
+        self._conn = "//connection_string"
+        self.user = "Afonso"
+
+    def get_database(self) -> None:
+        print(self.__database)
+
+    def _testing_conection(self) -> None:
+        print("Connection OK")
+
+class Repository(DatabaseConn):
+
+    def __init__(self) -> None:
+        super().__init__()
+        # print(self.user)
+        # print(self._conn)
+
+    def select(self) -> None:
+        self._testing_conection()
+        print("connecting to {}".format(self._conn))
+        print("SELECT * FROM table")
+
+repo = Repository()
+repo.select()
